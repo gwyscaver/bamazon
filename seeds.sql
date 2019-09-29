86611a2f5fa71d964526c604389e95511ce8ca8e
George Wyscaver <gwyscaver3@gmail.com>
2019-09-28 19:28:53 -0700

    completed schema abd seeds

---
+++ seeds.sql
@@ -0,0 +1,31 @@
+USE bamazon_db;
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES ("tent", "outdoors", 98.47, 25 );
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES ("sleeping bag", "outdoors", 36.80, 213)
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES ("latern", "outdoors", 19.97, 160);
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES ("rv class a", "transportation", 125000.00, 5);
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES ("smores", "food", 5.99, 100);
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES ("vegetarian hotdogs", "food", 6.99, 45);
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES ("cooler fridge", "outdoors", 153.00, 36);
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES ("portable stove", "outdoors", 43.88, 73);
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES ("cooking utensils", "housewares", 24.99, 87);
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES ("bed linens", "houswares", 57.99, 64);
